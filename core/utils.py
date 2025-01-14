dfd50bd5d584c2c7a9fa6a6c3d33a8ce2513f946
LA Tran <tranleanh.nt@gmail.com>
2025-01-14 15:48:29 +0900

    Add files via upload

--- core/utils.py
+++ core/utils.py
@@ -92,246 +92,16 @@ def preprocess_depth_img(cv_img):
     return img
 
 
-def preprocess_image_cv2_rancrop(img_A, img_B):
+def preprocess_guide(cv_img):
+    cv_img = cv2.resize(cv_img, RESHAPE)
+    img = np.array(cv_img)
+    return img
+
+
+def gen_hazy_clean_hint_soft_trans(img_A, img_B, teacher_model):
 
     t = estimate_transmission(img_A)
 
-    img_A = cv2.cvtColor(img_A, cv2.COLOR_BGR2RGB)
-    img_B = cv2.cvtColor(img_B, cv2.COLOR_BGR2RGB)
-
-    h, w, _ = img_A.shape
-
-    min_wh = np.amin([h, w])
-
-    # crop_sizes = [1600, 1800, 2000, 2200, 2400]
-    crop_sizes = [int(min_wh*0.4), int(min_wh*0.5), int(min_wh*0.6), int(min_wh*0.7), int(min_wh*0.8)]
-
-    images_A = []
-    images_B = []
-
-    for crop_size in crop_sizes:
-
-        x1, y1 = randint(1, w-crop_size-1), randint(1, h-crop_size-1)
-
-        cropA = img_A[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropA = cv2.resize(cropA, (RESHAPE))
-        cropA = np.array(cropA)
-        cropA = (cropA - 127.5) / 127.5
-
-        crop_t = t[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        crop_t = preprocess_depth_img(crop_t)
-
-        cropA = np.concatenate((cropA, crop_t), axis=2)
-
-        cropB = img_B[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropB = cv2.resize(cropB, (RESHAPE))
-        cropB = np.array(cropB)
-        cropB = (cropB - 127.5) / 127.5
-
-        images_A.append(cropA)
-        images_B.append(cropB)
-
-    img_A = cv2.resize(img_A, (RESHAPE))
-    img_A = np.array(img_A)
-    img_A = (img_A - 127.5) / 127.5
-
-    t = preprocess_depth_img(t)
-
-    img_A = np.concatenate((img_A, t), axis=2)
-
-    img_B = cv2.resize(img_B, (RESHAPE))
-    img_B = np.array(img_B)
-    img_B = (img_B - 127.5) / 127.5
-
-    images_A.append(img_A)
-    images_B.append(img_B)
-
-    return images_A, images_B
-
-
-
-def load_images_with_crop_data_aug(path, n_images):
-    if n_images < 0:
-        n_images = float("inf")
-    A_paths, B_paths = os.path.join(path, 'A'), os.path.join(path, 'B')
-    all_A_paths, all_B_paths = list_image_files(A_paths), list_image_files(B_paths)
-    images_A, images_B = [], []
-    images_A_paths, images_B_paths = [], []
-    cnt=0
-    for path_A, path_B in zip(all_A_paths, all_B_paths):
-
-        img_A, img_B = cv2.imread(path_A), cv2.imread(path_B)
-
-        processed_imgs_A, processed_imgs_B = preprocess_image_cv2_rancrop(img_A, img_B)
-
-        for imgA in processed_imgs_A: images_A.append(imgA)
-        for imgB in processed_imgs_B: images_B.append(imgB)
-
-        images_A_paths.append(path_A)
-        images_B_paths.append(path_B)
-
-        cnt+=1
-        print(cnt, n_images)
-        if len(images_A_paths) > n_images - 1: break
-
-    return {
-        'A': np.array(images_A),
-        'A_paths': np.array(images_A_paths),
-        'B': np.array(images_B),
-        'B_paths': np.array(images_B_paths)
-    }
-
-
-def preprocess_image_cv2_rancrop_flip(img_A, img_B):
-
-    t = estimate_transmission(img_A)
-    t_flip = cv2.flip(t, 1)
-
-    img_A = cv2.cvtColor(img_A, cv2.COLOR_BGR2RGB)
-    img_B = cv2.cvtColor(img_B, cv2.COLOR_BGR2RGB)
-
-    img_A_flip = cv2.flip(img_A, 1)
-    img_B_flip = cv2.flip(img_B, 1)
-
-    h, w, _ = img_A.shape
-
-    min_wh = np.amin([h, w])
-
-    # crop_sizes = [1600, 1800, 2000, 2200, 2400]
-    crop_sizes = [int(min_wh*0.4), int(min_wh*0.5), int(min_wh*0.6), int(min_wh*0.7), int(min_wh*0.8)]
-
-
-    images_A = []
-    images_B = []
-
-    for crop_size in crop_sizes:
-
-        x1, y1 = randint(1, w-crop_size-1), randint(1, h-crop_size-1)
-
-        # Original
-
-        cropA = img_A[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropA = cv2.resize(cropA, (RESHAPE))
-        cropA = np.array(cropA)
-        cropA = (cropA - 127.5) / 127.5
-
-        crop_t = t[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        crop_t = preprocess_depth_img(crop_t)
-
-        cropA = np.concatenate((cropA, crop_t), axis=2)
-
-        cropB = img_B[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropB = cv2.resize(cropB, (RESHAPE))
-        cropB = np.array(cropB)
-        cropB = (cropB - 127.5) / 127.5
-
-        images_A.append(cropA)
-        images_B.append(cropB)
-
-
-        # Horizontal Flip
-
-        cropA = img_A_flip[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropA = cv2.resize(cropA, (RESHAPE))
-        cropA = np.array(cropA)
-        cropA = (cropA - 127.5) / 127.5
-
-        crop_t = t_flip[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        crop_t = preprocess_depth_img(crop_t)
-
-        cropA = np.concatenate((cropA, crop_t), axis=2)
-
-        cropB = img_B_flip[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
-        cropB = cv2.resize(cropB, (RESHAPE))
-        cropB = np.array(cropB)
-        cropB = (cropB - 127.5) / 127.5
-
-        images_A.append(cropA)
-        images_B.append(cropB)
-
-
-    # Original
-
-    img_A = cv2.resize(img_A, (RESHAPE))
-    img_A = np.array(img_A)
-    img_A = (img_A - 127.5) / 127.5
-
-    t = preprocess_depth_img(t)
-
-    img_A = np.concatenate((img_A, t), axis=2)
-
-    img_B = cv2.resize(img_B, (RESHAPE))
-    img_B = np.array(img_B)
-    img_B = (img_B - 127.5) / 127.5
-
-    images_A.append(img_A)
-    images_B.append(img_B)
-
-
-    # Horizontal Flip
-
-    img_A = cv2.resize(img_A_flip, (RESHAPE))
-    img_A = np.array(img_A)
-    img_A = (img_A - 127.5) / 127.5
-
-    t = preprocess_depth_img(t_flip)
-
-    img_A = np.concatenate((img_A, t), axis=2)
-
-    img_B = cv2.resize(img_B_flip, (RESHAPE))
-    img_B = np.array(img_B)
-    img_B = (img_B - 127.5) / 127.5
-
-    images_A.append(img_A)
-    images_B.append(img_B)
-
-    return images_A, images_B
-
-
-
-def load_images_with_crop_flip_data_aug(path, n_images):
-    if n_images < 0:
-        n_images = float("inf")
-    A_paths, B_paths = os.path.join(path, 'A'), os.path.join(path, 'B')
-    all_A_paths, all_B_paths = list_image_files(A_paths), list_image_files(B_paths)
-    images_A, images_B = [], []
-    images_A_paths, images_B_paths = [], []
-    cnt=0
-
-    print('\n Loading & augmenting training data:')
-
-    for path_A, path_B in tqdm(zip(all_A_paths, all_B_paths), total=len(all_A_paths)):
-
-        img_A, img_B = cv2.imread(path_A), cv2.imread(path_B)
-
-        img_A = cv2.resize(img_A, RESHAPE)
-        img_B = cv2.resize(img_B, RESHAPE)
-
-        processed_imgs_A, processed_imgs_B = preprocess_image_cv2_rancrop_flip(img_A, img_B)
-
-        for imgA in processed_imgs_A: images_A.append(imgA)
-        for imgB in processed_imgs_B: images_B.append(imgB)
-
-        images_A_paths.append(path_A)
-        images_B_paths.append(path_B)
-
-        cnt+=1
-        # print(cnt, n_images)
-        if len(images_A_paths) > n_images - 1: break
-
-    return {
-        'A': np.array(images_A),
-        'A_paths': np.array(images_A_paths),
-        'B': np.array(images_B),
-        'B_paths': np.array(images_B_paths)
-    }
-
-
-
-
-def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
-
-    t = estimate_transmission(img_A)
     t_flip = cv2.flip(t, 1)
 
     img_A = cv2.cvtColor(img_A, cv2.COLOR_BGR2RGB)
@@ -349,6 +119,7 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
     images_B = []
     hints = []
     soft_labels = []
+    guides = []
 
 
     for crop_size in crop_sizes:
@@ -364,6 +135,8 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
         cropA = (cropA - 127.5) / 127.5
 
         crop_t = t[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
+        guide = 1 - preprocess_guide(crop_t)
+        guide = np.reshape(guide, (RESHAPE[0], RESHAPE[1], 1))
         crop_t = preprocess_depth_img(crop_t)
 
         cropA = np.concatenate((cropA, crop_t), axis=2)
@@ -384,6 +157,7 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
         images_B.append(cropB)
         hints.append(hint_fm[0])
         soft_labels.append(soft_label[0])
+        guides.append(guide)
 
 
         # Horizontal Flip of Crop
@@ -395,6 +169,8 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
         cropA = (cropA - 127.5) / 127.5
 
         crop_t = t_flip[y1:y1+crop_size,x1:x1+int(crop_size/h*w)]
+        guide = 1 - preprocess_guide(crop_t)
+        guide = np.reshape(guide, (RESHAPE[0], RESHAPE[1], 1))
         crop_t = preprocess_depth_img(crop_t)
 
         cropA = np.concatenate((cropA, crop_t), axis=2)
@@ -415,6 +191,7 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
         images_B.append(cropB)
         hints.append(hint_fm[0])
         soft_labels.append(soft_label[0])
+        guides.append(guide)
 
 
     # Original
@@ -423,6 +200,8 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
     img_A = cv2.resize(img_A, (RESHAPE))
     img_A = np.array(img_A)
     img_A = (img_A - 127.5) / 127.5
+    guide = 1 - preprocess_guide(t)
+    guide = np.reshape(guide, (RESHAPE[0], RESHAPE[1], 1))
     t = preprocess_depth_img(t)
     img_A = np.concatenate((img_A, t), axis=2)
 
@@ -441,6 +220,7 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
     images_B.append(img_B)
     hints.append(hint_fm[0])
     soft_labels.append(soft_label[0])
+    guides.append(guide)
 
 
     # Horizontal Flip
@@ -449,6 +229,8 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
     img_A = cv2.resize(img_A_flip, (RESHAPE))
     img_A = np.array(img_A)
     img_A = (img_A - 127.5) / 127.5
+    guide = 1 - preprocess_guide(t_flip)
+    guide = np.reshape(guide, (RESHAPE[0], RESHAPE[1], 1))
     t = preprocess_depth_img(t_flip)
     img_A = np.concatenate((img_A, t), axis=2)
 
@@ -467,12 +249,12 @@ def preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model):
     images_B.append(img_B)
     hints.append(hint_fm[0])
     soft_labels.append(soft_label[0])
+    guides.append(guide)
 
-    return images_A, images_B, hints, soft_labels
+    return images_A, images_B, hints, soft_labels, guides
 
 
-
-def load_images_with_crop_flip_data_aug_and_hint(path, n_images):
+def load_hazy_clean_hint_soft_trans(path, n_images):
 
     if n_images < 0:
         n_images = float("inf")
@@ -484,7 +266,7 @@ def load_images_with_crop_flip_data_aug_and_hint(path, n_images):
 
     print('\n Loading teacher network:')
     teacher_model = EDN_GTM()
-    weight_path = 'path/to/teacher_weights'
+    weight_path = 'path/to/teacher_weights.h5'
     teacher_model.load_weights(weight_path)
     print('-> Loading teacher network: Done.')
 
@@ -495,6 +277,8 @@ def load_images_with_crop_flip_data_aug_and_hint(path, n_images):
     images_B = []
     images_C = []
     images_D = []
+    images_E = []
+
 
     for path_A, path_B in tqdm(zip(all_A_paths, all_B_paths), total=len(all_A_paths)):
 
@@ -503,22 +287,24 @@ def load_images_with_crop_flip_data_aug_and_hint(path, n_images):
         img_A = cv2.resize(img_A, RESHAPE)
         img_B = cv2.resize(img_B, RESHAPE)
 
-        processed_imgs_A, processed_imgs_B, generated_hints, soft_labels = preprocess_image_cv2_rancrop_flip_make_hint(img_A, img_B, teacher_model)
+        processed_imgs_A, processed_imgs_B, generated_hints, soft_labels, guides = gen_hazy_clean_hint_soft_trans(img_A, img_B, teacher_model)
 
         for imgA in processed_imgs_A: images_A.append(imgA)
         for imgB in processed_imgs_B: images_B.append(imgB)
         for imgC in generated_hints: images_C.append(imgC)
         for imgD in soft_labels: images_D.append(imgD)
+        for imgE in guides: images_E.append(imgE)
 
         images_A_paths.append(path_A)
         images_B_paths.append(path_B)
 
         cnt+=1
-        if len(images_A_paths) > n_images - 1: break
-
+        if cnt >= n_images: break
+        
     return {
         'A': np.array(images_A),
         'B': np.array(images_B),
         'C': np.array(images_C),
-        'D': np.array(images_D)
+        'D': np.array(images_D),
+        'E': np.array(images_E)
     }
